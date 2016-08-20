7ad0119fbdab57323c66f22bb59b68668c5e64d9
Eric Bai <heyericbai@gmail.com>
2016-08-20 10:17:17 -0400

    updated db migration to have a fixed hash for the default lock code, generating a new lock bcrypt hash each time yields an invalid checksum that throws an error on server restart

--- grails-app/migrations/lock-code.groovy
+++ grails-app/migrations/lock-code.groovy
@@ -1,10 +1,8 @@
 import org.textup.Constants
 
 databaseChangeLog = {
-
-	String encodedDefault = ctx.getBean("passwordEncoder")
-		.encodePassword(Constants.DEFAULT_LOCK_CODE, null)
-
+	// bcrypt encoded default is 8888
+	String encodedDefault = '$2a$10$TIIecZI1r8Dh8T3Rd4PFnOj21fA94JkOuYi51kH.Wk2KHYNxQL4fu'
 	changeSet(author: "eb27 (generated)", id: "1471631452247-1") {
 		addColumn(tableName: "staff") {
 			column(name: "lock_code", type: "varchar(255)",
