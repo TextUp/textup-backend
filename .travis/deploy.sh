b26bba7f8d1d515d5456dbb10e7675bcb4fd2fa6
Eric Bai <eric@ericbai.com>
2018-03-04 18:15:05 -0500

    disabled strict key checking for ssh commands to avoid build hanging when asking to add host to known_hosts

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,5 +9,5 @@ war_directory=$4
 war_name=$5
 remote_script=$6
 
-scp -i ${identity} ${war_directory}/${war_name} ${user}@${host}:~
-ssh -i ${identity} ${user}@${host} 'bash -s' -- < ${remote_script} ${war_name}
+scp -oStrictHostKeyChecking=no -i ${identity} ${war_directory}/${war_name} ${user}@${host}:~
+ssh -oStrictHostKeyChecking=no -i ${identity} ${user}@${host} 'bash -s' -- < ${remote_script} ${war_name}
